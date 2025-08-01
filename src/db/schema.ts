d31345ce0d6580aba368231ef3453985be622bd7
Abhimanyu jangid <87138461+abhimanyujangid@users.noreply.github.com>
2025-08-01 11:22:54 +0530

    feat: add agents management with TRPC integration; implement loading and error states for agent views

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -5,6 +5,7 @@ import {
   boolean,
   integer,
 } from "drizzle-orm/pg-core";
+import { nanoid } from "nanoid";
 
 export const user = pgTable("user", {
   id: text("id").primaryKey(),
@@ -59,9 +60,22 @@ export const verification = pgTable("verification", {
   value: text("value").notNull(),
   expiresAt: timestamp("expires_at").notNull(),
   createdAt: timestamp("created_at").$defaultFn(
-    () => /* @__PURE__ */ new Date(),
+    () => /* @__PURE__ */ new Date()
   ),
   updatedAt: timestamp("updated_at").$defaultFn(
-    () => /* @__PURE__ */ new Date(),
+    () => /* @__PURE__ */ new Date()
   ),
+});
+
+export const agents = pgTable("agents", {
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => nanoid()),
+  name: text("name").notNull(),
+  userId: text("user_id")
+    .notNull()
+    .references(() => user.id, { onDelete: "cascade" }),
+  instruction: text("instruction").notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at").notNull().defaultNow(),
 });
--- src/modules/home/ui/view/home-view.tsx
+++ src/modules/home/ui/view/home-view.tsx
@@ -1,21 +1,9 @@
 "use client";
 
-import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
-
-
 export default function HomeView() {
-  const trpc = useTRPC();
-  const { data,  } = useQuery(
-    trpc.hello.queryOptions({
-      text:'Antonion '
-    })
-  ); 
-  
-
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-     
+     Home View
     </div>
   );
 }
