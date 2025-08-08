60151522d5e317d74ed427ca8b0136cb5a5a22ae
Abhimanyu jangid <87138461+abhimanyujangid@users.noreply.github.com>
2025-08-08 08:03:47 +0530

    fix: update layout class for MeetingView to improve responsiveness

--- src/modules/meetings/ui/views/meetings-view.tsx
+++ src/modules/meetings/ui/views/meetings-view.tsx
@@ -19,7 +19,8 @@ export const MeetingView = () => {
   );
 
   return (
-    <div className="overflow-x-scroll">
+    <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4
+    ">
       <DataTable data={data.items} columns={columns}  />
     </div>
   )
