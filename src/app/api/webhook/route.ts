df3257f7d28753ac37a0ce64f3823adb831a0608
Abhimanyu jangid <87138461+abhimanyujangid@users.noreply.github.com>
2025-08-30 10:16:28 +0530

    feat: add webhook route for handling video call events; integrate OpenAI Realtime API and update meeting status

---
+++ src/app/api/webhook/route.ts
@@ -0,0 +1,114 @@
+import {
+    CallEndedEvent,
+    CallTranscriptionReadyEvent,
+    CallSessionParticipantLeftEvent,
+    CallRecordingReadyEvent,
+    CallSessionStartedEvent
+} from "@stream-io/video-react-sdk";
+
+// Drizzle ORM
+import { and , eq, not } from "drizzle-orm";
+import { NextRequest, NextResponse } from "next/server";
+
+import { agents, meetings } from "@/db/schema";
+import  streamVideo  from "@/lib/stream-video";
+import { db } from "@/db";
+
+
+function verifySignatureWithSDK(body: string, signature: string): boolean {
+    return streamVideo.verifyWebhook(body, signature);
+}
+
+export async function POST(req: NextRequest) {
+    const signature = req.headers.get("x-signature") || "";
+    const apiKey = req.headers.get("x-api-key") || "";
+
+    if (!apiKey || !signature) {
+        return NextResponse.json({ error: "Missing API key or signature" }, { status: 401 });
+    }
+
+    const body = await req.text();
+
+    if (!verifySignatureWithSDK(body, signature)) {
+        return NextResponse.json({ error: "Invalid signature" }, { status: 401 });
+    }
+
+    let payload : unknown;
+
+    try {
+        payload = JSON.parse(body) as Record<string, unknown>;
+    } catch (error) {
+        return NextResponse.json({ error: "Invalid JSON" }, { status: 400 });
+    }
+
+    const eventType = (payload as Record<string, unknown>)?.type;
+
+    if (eventType === "call.session_started") {
+        const event  = payload as CallSessionStartedEvent;
+        const meetingId = event.call.custom?.meetingid;
+
+        if(!meetingId){
+            return NextResponse.json({ error: "Missing meeting ID" }, { status: 400 });
+        }
+
+        const [existingMeeting] = await db
+            .select()
+            .from(meetings)
+            .where(
+                and(
+                    eq(meetings.id, meetingId),
+                    not(eq(meetings.status, 'completed')),
+                    not(eq(meetings.status, 'active')),
+                    not(eq(meetings.status, 'cancelled')),
+                    not(eq(meetings.status, 'processing'))
+                )
+            );
+
+        if (!existingMeeting) {
+            return NextResponse.json({ error: "Meeting not found" }, { status: 404 });
+        }
+
+        await db
+        .update(meetings)
+        .set({
+            status: 'active',
+            startedAt: new Date()
+        })
+        .where(eq(meetings.id, existingMeeting.id));
+
+        const [existingAgent] = await db
+            .select()
+            .from(agents)
+            .where(eq(agents.id, existingMeeting.agentId));
+
+        if (!existingAgent) {
+            return NextResponse.json({ error: "Agent not found" }, { status: 404 });
+        }
+
+
+        const call = streamVideo.video.call('default', meetingId);
+
+        const realtimeClient = await streamVideo.video.connectOpenAi({
+            call,
+            openAiApiKey: process.env.OPENAI_API_KEY!,
+            agentUserId: existingAgent.id
+        });
+
+        realtimeClient.updateSession({
+            instructions:existingAgent.instruction
+        })
+    }else if(eventType === 'call.session_participant_left') {
+        const event = payload as CallSessionParticipantLeftEvent;
+        const meetingId = event.call_cid.split(':')[1];
+
+        if(!meetingId){
+            return NextResponse.json({ error: "Missing meeting ID" }, { status: 400 });
+        }
+
+        const call =  streamVideo.video.call('default', meetingId);
+        await call.end();
+    }
+    return NextResponse.json({ status: 'ok' });
+
+    // Handle the webhook event
+}
